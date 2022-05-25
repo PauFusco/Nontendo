31eb4bd2dcf63506f401d5e1b52a8357e55a6d04
unsanfer89 <84281218+unsanfer89@users.noreply.github.com>
2022-05-25 18:26:47 +0200

    ?
    ?

--- Windjammers/0.6/Windjammers/Windjammers/Menu.cpp
+++ Windjammers/0.6/Windjammers/Windjammers/Menu.cpp
@@ -6,3 +6,49 @@
 #include "ModuleFadeToBlack.h"
 
 #include "Menu.h"
+
+Menu::Menu(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+Menu::~Menu()
+{
+
+}
+
+// Load assets
+bool Menu::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/Sprites/introwindjammers.png");
+	App->audio->PlayMusic("Assets/Music/Beach.ogg", 1.0f);
+
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+
+	return ret;
+}
+
+Update_Status SceneIntro::Update()
+{
+	if (App->input->keys[SDL_SCANCODE_SPACE] == KEY_STATE::KEY_DOWN)
+	{
+		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
+
+	}
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status SceneIntro::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
